cc4a629317bc005a5de8e7bc9850c9eda46a8c72
Jared Wasserman <jared.wasserman@thea.energy>
2024-06-06 14:57:59 -0400

    Updated display functions

--- RadialBuild_v2.py
+++ RadialBuild_v2.py
@@ -16,19 +16,21 @@ canvas.create_text(10, 10, text="EOS Radial Build", font = "Consolas 36", anchor
 canvas.create_text(10, 55, text="as of 5/27/2024", font = "Consolas 20", anchor="nw")
 
 f = open("Radial_Layers.txt", 'r')
-layers = [line.rstrip().split(',') for line in f]
-# print(layers)
+layers = [line.strip().split(',') for line in f]
+print(layers)
 
 segmentHeight = 60
-xStart = 400
-yStart = 350
-scale = 10
+xStart = 100
+yStart = 300
+
+xscale = 7.5
+yscale = 11
 xList = []
 widthList = []
 segList = []
 
 pw = 10
-px1 = xStart-pw*scale
+px1 = xStart-pw*xscale
 segList.append(["Plasma",pw, px1, "PaleGreen1"])
 xList.append(px1)
 widthList.append(pw)
@@ -44,29 +46,29 @@ def rectangle(index):
     label = "{} ({} cm)".format(name, width)
     x1 = xval
     y1 = yStart
-    x2 = xval + width * scale
-    y2 = yStart + segmentHeight * scale
-
+    x2 = xval + width * xscale
+    y2 = yStart + segmentHeight * yscale
+    
     mid = (x2+x1)/2
+    labelAngle = 60
 
     canvas.create_rectangle(x1, y1, x2, y2, fill=color)
    
     if width < 5:
         ticLen = 50
-        xtic = mid+ticLen*math.sin(45)
-        ytic = y1-ticLen*math.sin(45)
+        xtic = mid+ticLen*math.cos(math.radians(labelAngle))
+        ytic = y1-ticLen*math.sin(math.radians(labelAngle))
         canvas.create_line(mid, y1, xtic, ytic)
-        canvas.create_text(mid, y1, text=label, fill="Black", font="Consolas 16", anchor="sw", angle=45)
+        canvas.create_text(mid, y1, text=label, fill="Black", font="Consolas 12", anchor="sw", angle=labelAngle)
     else:
         canvas.create_text(mid, (y1+y2)/2, text=label, font="Consolas 22", angle=90)
 
-# def addSegment(name, width, color="snow"):
 def addSegment(seg):
     name = seg[0]
-    width = int(seg[1])
+    width = float(seg[1])
     color = seg[2]
 
-    xVal = xList[-1] + widthList[-1] * scale
+    xVal = xList[-1] + widthList[-1] * xscale
     segList.append([name, width, xVal, color])
     xList.append(xVal)
     widthList.append(width)
@@ -81,20 +83,25 @@ for val in range(len(segList)):
 
 
 ## Ruler
-rLength = 1000
+rLength = 220
 rStep = 10
-rMax = int(rLength/rStep)
-rEnd = rLength + xStart
-ticY1 = 990
+
+ticY1 = 1000
 ticLen = 10
 ticY2 = ticY1 - ticLen
-canvas.create_line(xStart, ticY1, rEnd, ticY1)
+ticY3 = ticY1 - ticLen/2
+canvas.create_line(xStart, ticY1, rLength*xscale + xStart, ticY1)
 
-for val in range(0, rMax, rStep):
-    x = xStart + val * rStep
+for val in range(0, rLength+rStep, rStep):
+    print(val)
+    x = xStart + val * xscale
     canvas.create_line(x, ticY1, x, ticY2)
-    canvas.create_text(x, ticY2+30, text=rStep*val/scale, font="Consolas 12")
+    canvas.create_text(x, ticY2+30, text=val, font="Consolas 12")
 
+    if val < rLength:
+        for j in range(1, rStep):
+            x2 = x + j*xscale
+            canvas.create_line(x2, ticY1, x2, ticY3 )
 
 
 root.mainloop()
